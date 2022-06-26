ca163dd00a5f89aa4daf41d60dd40f573f572e90
Libardo Antonio Morales Galeano <lianmgaleano@gmail.com>
2022-06-26 11:29:44 -0500

    Añade permisos sobre los usuarios

--- logica/logic.py
+++ logica/logic.py
@@ -35,16 +35,27 @@ def get_about_this(VERSION):
 #########################################################
 
 #HELP
-def get_help_message ():
+def get_help_message (user_id):
     response = (
                 "Estos son los comandos y órdenes disponibles:\n"
                 "\n"
                 "*/start* - Inicia la interacción con el bot (obligatorio)\n"
                 "*/help* - Muestra este mensaje de ayuda\n"
                 "*/about* - Muestra detalles de esta aplicación\n"
+                )
+    
+    if check_admin(user_id):        
+        response += (
                 "*registrar mecánico* - Permite el registro de un mecánico\n"
                 "*registrar dueño* - Permite el registro de un dueño\n"
+                "*registrar vehículo* - Permite el registro de un vehículo\n"
+                "*asignar mecánico* - Permite asignar un mecanico a un vehículo\n"
                 )
+    elif check_mechanic(user_id):
+        response += (
+                "*registrar niveles* - Registrar los niveles de líquidos de los vehículos\n"
+                )
+    
     return response
 
 ########################################################
@@ -55,3 +66,17 @@ def get_fallback_message (text):
     return response
 
 ########################################################
+
+#Verifica si el id del usuario conectado al chat pertenece a la empresa
+def check_admin(user_id):
+    admins = [1562128595]
+    return user_id in admins
+
+#########################################################
+
+#Verifica si el id del usuario conectado al chat pertenece al listado de mecanicos
+def check_mechanic(user_id):
+    mecanicos = [1562128595]
+    return user_id in mecanicos
+
+#########################################################
